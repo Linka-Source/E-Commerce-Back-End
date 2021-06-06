274950698a30cb5d023a4909644d688301b701fb
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-06-06 20:27:11 +0930

    remembered the async!

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,7 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
 try {
@@ -15,7 +15,7 @@ try {
 }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 try {
@@ -32,7 +32,7 @@ try {
 }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
 Category.create({
   category_name: req.body.category_name
@@ -44,11 +44,11 @@ Category.create({
 });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 
