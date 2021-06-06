2a435f6f2165bf420b0401340e87c87a312ad570
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-06-06 20:13:41 +0930

    find category by ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,6 +18,18 @@ try {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+try {
+  const categoryData = await Category.findByPk(req.params.id. {
+    include: [{ model:Product}]
+  });
+  if (!categoryData) {
+    res.status(404).json({ message: 'Did not find category with this ID.'});
+    return;
+  }
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.post('/', (req, res) => {
