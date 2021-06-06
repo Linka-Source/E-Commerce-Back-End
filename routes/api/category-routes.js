b7141f9863b058d97a03365499db903d92055c70
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-06-06 22:21:20 +0930

    delete update

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
 try {
   const categoryData = await Category.findAll({ 
-    include: [{model: Product}]});
+    include: [{ model: Product }]});
     res.status(200).json(categoryData);
 } catch (err) {
   res.status(500).json(err);
@@ -19,8 +19,8 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 try {
-  const categoryData = await Category.findByPk(req.params.id. {
-    include: [{ model:Product}]
+  const categoryData = await Category.findByPk(req.params.id, {
+    include: [{ model:Product }]
   });
   if (!categoryData) {
     res.status(404).json({ message: 'Did not find category matching this ID.'});
