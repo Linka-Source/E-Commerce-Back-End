906a602f3dbe689a040cda057ff6e2f916e7e408
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-06-06 20:05:34 +0930

    find all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,13 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+try {
+  const categoryData = await Category.findAll({ 
+    include: [{model: Product}]});
+    res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.get('/:id', (req, res) => {
@@ -26,3 +33,4 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+
